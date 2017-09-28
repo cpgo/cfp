9fdae6b94f1fec9a7eb5da5addcca7ed3e28b101
xvw <xavier.vdw@gmail.com>
2017-09-28 23:32:44 +0200

    Add a first pass on the Email

--- lib/cfp_web/controllers/slack_controller.ex
+++ lib/cfp_web/controllers/slack_controller.ex
@@ -6,15 +6,23 @@ defmodule CfpWeb.SlackController do
   alias Cfp.Slack.Invitation
 
   def index(conn, _params) do
-    render(conn, "index.html")
+    changeset = Invitation.changeset(%Invitation{})
+    render(conn, "index.html", changeset: changeset)
   end
 
   def new(conn, params), do: index(conn, params)
 
-  def create(conn, email: email) do
+  def create(conn, %{"invitation" => params }) do
+    {changeset, flash, message} =
+      case Invitation.perform(params) do
+	{:ok, {change, email}} ->
+	  {change, :info, email}
+	{:error, {change, error_message}} ->
+	  {change, :error, error_message}
+      end
     conn
-    |> put_flash(:info, email)
-    |> render("index.html")
+    |> put_flash(flash, message)
+    |> render("index.html", changeset: changeset)
   end
   
 end
--- lib/slack/invitation.ex
+++ lib/slack/invitation.ex
@@ -14,8 +14,21 @@ defmodule Cfp.Slack.Invitation do
   """
   def changeset(invitation, params \\ %{}) do
     invitation
+    |> cast(params, [:email])
     |> validate_required([:email])
     |> validate_format(:email, ~r/@/)
+  end
+
+  @doc """
+  perform validation on user parameters
+  """
+  def perform(parameters) do
+    change = changeset(%__MODULE__{}, parameters)
+    if change.valid? do
+      {:ok, {change, change.changes.email}}
+    else
+      {:error, {change, Util.pop_error(change)}}
+    end
   end
 
 end
